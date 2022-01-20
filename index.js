e3779cd6fe81c0696c5d31df78aca765253142b1
dragon531024 <dragon531024@gmail.com>
2022-01-20 12:44:41 +0700

    master

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ jsonReader("./data1.json", (err, data) => {
   }
 });
 
-fs.writeFile('./newCustomer.json', JSON.stringify(newObject, null, 2), err => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('File successfully written!')
-    }
-})
+// fs.writeFile('./newCustomer.json', JSON.stringify(newObject, null, 2), err => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log('File successfully written!')
+//     }
+// })
